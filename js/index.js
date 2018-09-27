0f67717eeaffc614eb16cbbc28412a6d9e20954c
Sukruth M K <suk_mallur@yahoo.co.in>
2018-09-26 23:28:37 -0700

    Implemented form submit using IFTT

--- js/index.js
+++ js/index.js
@@ -3,17 +3,17 @@ $(document).ready(function(){
     var checkScrollBar = function(){
         var element = $(this);
         if(element.scrollTop() > 1) {
-            console.log("coming if");
             $("#navbar").removeClass('bg-transparent');
             $("#navbar").addClass('bg-purple');
         } else {
-            console.log("coming else");
             $("#navbar").removeClass('bg-purple');
             $("#navbar").addClass('bg-transparent');
         }
     }
 
     $(window).on('load resize scroll', checkScrollBar);
+
+    $("#aria-input").focus();
 
      // Add smooth scrolling on all links inside the navbar
      $("#navbar a").on('click', function(event) {
@@ -36,4 +36,35 @@ $(document).ready(function(){
              });
            }  // End if
      });
+
+     $("form").submit(function(e){
+        e.preventDefault();
+        console.log("form submit");
+        // Make request to IFTTT
+        // Using YQL and JSONP
+        $.ajax({
+            url: "https://maker.ifttt.com/trigger/{form_submit}/with/key/k7eFYJauLSuxWdgHSRaAuC8F2hOaSC0EsztaclQOcbk",
+
+            // The name of the callback parameter, as specified by the YQL service
+            jsonp: "callback",
+
+            // Tell jQuery we're expecting JSONP
+            dataType: "jsonp",
+
+            // Tell YQL what we want and that we want JSON
+            data: {
+                value1: jQuery("#contact-name").val(),
+                value2: jQuery("#contact-email").val(),
+                value3: jQuery("#contact-message").val(),
+                format: "json"
+            },
+
+            // Work with the response
+            success: function( response ) {
+                console.log( "sent mail" ); // server response
+
+                $('form').trigger("reset");
+            }
+        });
+    });
 });
